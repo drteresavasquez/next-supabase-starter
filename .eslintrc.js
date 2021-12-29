dba1e63dff1f6cd41212d5de5521e6ad0c030e2c
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2021-12-29 09:40:43 -0600

    testing husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
   extends: ["next", "next/core-web-vitals"],
-  // env: {
-  //   browser: true,
-  //   es2021: true,
-  // },
-  // extends: ['prettier', 'airbnb'],
-  // parserOptions: {
-  //   ecmaFeatures: {
-  //     jsx: true,
-  //   },
-  //   ecmaVersion: 12,
-  //   sourceType: 'module',
-  // },
-  // plugins: ['prettier', 'jest'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['prettier', 'airbnb'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['prettier', 'jest'],
   rules: {
+    "no-unused-vars": "error",
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/jsx-one-expression-per-line': [0],
     'no-console': [1, { allow: ['error', 'warn'] }],
